116109f408f62eb9ba6d7c162c8d9e326b50542b
Ralf Westphal <info@ralfw.de>
2015-02-23 16:54:46 +0100

    lesson 1.2

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -1,5 +1,6 @@
 using System;
 using Akka.Actor;
+using WinTail.Messages;
 
 namespace WinTail
 {
@@ -10,6 +11,8 @@ namespace WinTail
     class ConsoleReaderActor : UntypedActor
     {
         public const string ExitCommand = "exit";
+		public const string StartCommand = "start";
+
         private ActorRef _consoleWriterActor;
 
         public ConsoleReaderActor(ActorRef consoleWriterActor)
@@ -19,23 +22,61 @@ namespace WinTail
 
         protected override void OnReceive(object message)
         {
-            var read = Console.ReadLine();
-            if (!string.IsNullOrEmpty(read) && read.ToLowerInvariant().Equals(ExitCommand))
-            {
-                // shut down the system (acquire handle to system via
-                // this actors context)
-                Context.System.Shutdown();
-                return;
-            }
+			if (message.Equals (StartCommand))
+				DoPrintInstructions ();
+			else if (message is InputError) {
+				_consoleWriterActor.Tell (message);
+			}
 
-            // send input to the console writer to process and print
-            // YOU NEED TO FILL IN HERE
-			_consoleWriterActor.Tell (read);
-
-            // continue reading messages from the console
-            // YOU NEED TO FILL IN HERE
-			Self.Tell ("continue");
+			GetAndValidateInput ();
         }
 
+
+
+		private void DoPrintInstructions()
+		{
+			Console.WriteLine("Write whatever you want into the console!");
+			Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
+			Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+		}
+
+
+		private void GetAndValidateInput()
+		{
+			var message = Console.ReadLine();
+			if (string.IsNullOrEmpty(message))
+			{
+				// signal that the user needs to supply an input, as previously
+				// received input was blank
+				Self.Tell(new NullInputError("No input received."));
+			}
+			else if (message.Equals(ExitCommand))
+			{
+				// shut down the entire actor system (allows the process to exit)
+				Context.System.Shutdown();
+			}
+			else
+			{
+				var valid = IsValid(message);
+				if (valid)
+				{
+					_consoleWriterActor.Tell(new InputSuccess("Thank you! Message was valid."));
+
+					// continue reading messages from console
+					Self.Tell(new ContinueProcessing());
+				}
+				else
+				{
+					Self.Tell(new ValidationError("Invalid: input had odd number of characters."));
+				}
+			}
+		}
+
+
+		private static bool IsValid(string message)
+		{
+			var valid = message.Length % 2 == 0;
+			return valid;
+		}
     }
 }
--- src/Unit-1/DoThis/ConsoleWriterActor.cs
+++ src/Unit-1/DoThis/ConsoleWriterActor.cs
@@ -11,24 +11,24 @@ namespace WinTail
     {
         protected override void OnReceive(object message)
         {
-            var msg = message as string;
+			if (message is Messages.InputError)
+			{
+				var msg = message as Messages.InputError;
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine(msg.Reason);
+			}
+			else if (message is Messages.InputSuccess)
+			{
+				var msg = message as Messages.InputSuccess;
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine(msg.Reason);
+			}
+			else
+			{
+				Console.WriteLine("{0} ({1})", message, message.GetType().Name);
+			}
 
-            // make sure we got a message
-            if (string.IsNullOrEmpty(msg))
-            {
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Please provide an input.\n");
-                return;
-            }
-
-            // if message has even # characters, display in red; else, green
-            var even = msg.Length % 2 == 0;
-            var color = even ? ConsoleColor.Red : ConsoleColor.Green;
-            var alert = even ? "Your string had an even # of characters.\n" : "Your string had an odd # of characters.\n";
-            Console.ForegroundColor = color;
-            Console.WriteLine(alert);
-            Console.ResetColor();
-
+			Console.ResetColor();
         }
     }
 }
