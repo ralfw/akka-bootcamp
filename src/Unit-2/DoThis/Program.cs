7ab81b9f185faad8b1384f335d1d07757ef10bf8
Ralf Westphal <info@ralfw.de>
2015-03-01 08:53:45 +0100

    lesson 2.1

--- src/Unit-2/DoThis/Program.cs
+++ src/Unit-2/DoThis/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Windows.Forms;
 using Akka.Actor;
+using ChartApp.Actors;
+
+using System.Configuration;
+using Akka.Configuration.Hocon;
+
 
 namespace ChartApp
 {
@@ -18,7 +23,11 @@ namespace ChartApp
         [STAThread]
         static void Main()
         {
-            ChartActors = ActorSystem.Create("ChartActors");
+            var section = (AkkaConfigurationSection)ConfigurationManager.GetSection("akka");
+            var config = section.AkkaConfig;
+            ChartActors = ActorSystem.Create("ChartActors", config);
+            //ChartActors = ActorSystem.Create("ChartActors");
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Main());
