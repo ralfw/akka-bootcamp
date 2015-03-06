7c48ae89145296d6593fe04f8c91499edb363ba4
Ralf Westphal <info@ralfw.de>
2015-03-06 13:47:37 +0100

    lesson 2.4

--- src/Unit-2/DoThis/Actors/ChartingActor.cs
+++ src/Unit-2/DoThis/Actors/ChartingActor.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Windows.Forms.DataVisualization.Charting;
 using Akka.Actor;
 
+using System.Windows.Forms;
+
 namespace ChartApp.Actors
 {
     public class ChartingActor : ReceiveActor
@@ -20,6 +22,8 @@ namespace ChartApp.Actors
 
 
         #region Messages
+        public class TogglePause { }
+
         public class AddSeries
         {
             public AddSeries(Series series)
@@ -57,22 +61,58 @@ namespace ChartApp.Actors
 
         private readonly Chart _chart;
         private Dictionary<string, Series> _seriesIndex;
+        private readonly Button _pauseButton;
 
-        public ChartingActor(Chart chart) : this(chart, new Dictionary<string, Series>()) {}
-        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex)
+        public ChartingActor(Chart chart, Button pauseButton)
+            : this(chart, new Dictionary<string, Series>(), pauseButton){}
+        public ChartingActor(Chart chart, Dictionary<string, Series> seriesIndex, Button pauseButton)
         {
             _chart = chart;
             _seriesIndex = seriesIndex;
+            _pauseButton = pauseButton;
+            Charting();
+        }
 
+
+        private void Charting()
+        {
             Receive<InitializeChart>(ic => HandleInitialize(ic));
             Receive<AddSeries>(addSeries => HandleAddSeries(addSeries));
-
             Receive<RemoveSeries>(removeSeries => HandleRemoveSeries(removeSeries));
             Receive<Metric>(metric => HandleMetrics(metric));
+
+            //new receive handler for the TogglePause message type
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(true);
+                Become(Paused, false);
+            });
+        }
+
+        private void Paused()
+        {
+            Receive<Metric>(metric => HandleMetricsPaused(metric));
+            Receive<TogglePause>(pause =>
+            {
+                SetPauseButtonText(false);
+                Unbecome();
+            });
         }
 
 
         #region Individual Message Type Handlers
+        private void HandleMetricsPaused(Metric metric)
+        {
+            if (!string.IsNullOrEmpty(metric.Series) && _seriesIndex.ContainsKey(metric.Series))
+            {
+                var series = _seriesIndex[metric.Series];
+                series.Points.AddXY(xPosCounter++, 0.0d); //set the Y value to zero when we're paused
+                while (series.Points.Count > MaxPoints) series.Points.RemoveAt(0);
+                SetChartBoundaries();
+            }
+        }
+
+
         private void HandleInitialize(InitializeChart ic)
         {
             if (ic.InitialSeries != null)
@@ -158,6 +198,11 @@ namespace ChartApp.Actors
                 area.AxisY.Minimum = minAxisY;
                 area.AxisY.Maximum = maxAxisY;
             }
+        }
+
+        private void SetPauseButtonText(bool paused)
+        {
+            _pauseButton.Text = string.Format("{0}", !paused ? "PAUSE ||" : "RESUME ->");
         }
     }
 }
